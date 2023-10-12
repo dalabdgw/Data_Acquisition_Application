b4d785bade87044c7dcfae955fd3c279e9ae7f43
dalabdgw <135303032+dalabdgw@users.noreply.github.com>
2023-10-12 18:52:06 +0900

    server modify

--- deploy version/application servertest/lib/server_info.dart
+++ deploy version/application servertest/lib/server_info.dart
@@ -3,6 +3,6 @@
 // database IP
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-String AWSRDSIP ='http://127.0.0.1:50000';
+String AWSRDSIP ='http://annotationdb.cszainnq3jgr.ap-northeast-2.rds.amazonaws.com:50000';
 //String AWSRDSIP = dotenv.get('DB_RDS_CONNECTION');
 String AWSS3IP = dotenv.get('S3_IP');
